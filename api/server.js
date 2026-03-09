2278fb1ecacf4d45075723d7f38c05a2494d096c
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-03-09 14:52:31 +0000

    added security, ratelimit, IP limiting

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+
 const app = express()
 const port = process.env.PORT || 8080
-
+//middleware
+app.use(helmet())
+/*app.use(cors({
+    origin: 'https://eduardteodor.co.uk'
+}))*/
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}))
 app.use(express.json())
 
 app.use('/auth', require('./routes/auth'))
 app.use('/blogs', require('./routes/blogs'))
 app.use('/admin', require('./routes/admin'))
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
 
 app.get('/', (req, res) => {
     res.status(200).json({
@@ -17,3 +27,5 @@ app.get('/', (req, res) => {
         timestamp: new Date().toISOString()
     })
 })
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
