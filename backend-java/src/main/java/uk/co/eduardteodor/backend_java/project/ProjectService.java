0ef40ef74cd611e93354584df22eab095569e1d3
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-15 21:01:06 +0100

    feat: add project service

---
+++ backend-java/src/main/java/uk/co/eduardteodor/backend_java/project/ProjectService.java
@@ -0,0 +1,66 @@
+package uk.co.eduardteodor.backend_java.project;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProjectService {
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {this.projectRepository = projectRepository;}
+
+    //--------------------------------
+    //Public - anyone can call this
+    //--------------------------------
+
+    //Get all Published projects
+    public List<Project> getAllPublishedProjects() {
+        return projectRepository.findByPublishedTrue();
+    }
+
+    //Get all Featured projects published projects
+    public List<Project> getAllFeaturedAndPublishedProject() {
+        return projectRepository.findByFeaturedTrueAndPublishedTrue();
+    }
+    //Get top 3 Featured projects
+    public List<Project> getLatestProjects() {
+        return projectRepository.findTop3ByFeaturedTrueAndPublishedTrue();
+    }
+
+    //-----------------------------------
+    //Admin - ONLY ADMIN CAN ACCESS THIS
+    //-----------------------------------
+
+    //Get all projects
+    public List<Project> getAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    //Create Project
+    public Project createProject(Project project) {
+        return projectRepository.save(project);
+    }
+
+    //Update Project
+    public Project updateProject(UUID id, Project updated){
+        Project existing = projectRepository.findById(id).orElseThrow(() -> new RuntimeException("Post with id " + id + " not found"));
+
+        existing.setTitle(updated.getTitle());
+        existing.setDescription(updated.getDescription());
+        existing.setSummary(updated.getSummary());
+        existing.setTechStack(updated.getTechStack());
+        existing.setGithubUrl(updated.getGithubUrl());
+        existing.setLiveUrl(updated.getLiveUrl());
+        existing.setStatus(updated.getStatus());
+        existing.setFeatured(updated.isFeatured());
+        existing.setPublished(updated.isPublished());
+
+        return projectRepository.save(existing);
+    }
+    //Delete Project
+    public void deleteProject(UUID id) {projectRepository.deleteById(id);}
+
+}
