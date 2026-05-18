8e677810b7db6978265818533db3f20f4aabe6a9
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-18 17:05:20 +0100

    fix: correct column mapping & missing annotation

--- backend-java/src/main/java/uk/co/eduardteodor/backend_java/user/UserRepository.java
+++ backend-java/src/main/java/uk/co/eduardteodor/backend_java/user/UserRepository.java
@@ -3,10 +3,10 @@ package uk.co.eduardteodor.backend_java.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 }
