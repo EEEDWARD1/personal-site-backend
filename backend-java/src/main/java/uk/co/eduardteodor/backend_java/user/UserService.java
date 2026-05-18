09345d8f41231691647af79ca3b97fa747d70a7e
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-18 17:03:53 +0100

    feat: add user service

---
+++ backend-java/src/main/java/uk/co/eduardteodor/backend_java/user/UserService.java
@@ -0,0 +1,18 @@
+package uk.co.eduardteodor.backend_java.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
