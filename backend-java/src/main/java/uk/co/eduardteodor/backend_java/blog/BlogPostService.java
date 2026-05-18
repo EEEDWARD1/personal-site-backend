7d852f5af2dcaa554b38494410b6cbf3138e0cd3
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-18 17:44:09 +0100

    refactor: refactor: rewrite blog service for sprint 2 public endpoints

--- backend-java/src/main/java/uk/co/eduardteodor/backend_java/blog/BlogPostService.java
+++ backend-java/src/main/java/uk/co/eduardteodor/backend_java/blog/BlogPostService.java
@@ -14,47 +14,15 @@ public class BlogPostService {
         this.blogPostRepository = blogPostRepository;
     }
 
-    //--------------------------------
-    //Public - anyone can call this
-    //--------------------------------
-    public List<BlogPost> getALlPublishedPosts(){
+    public List<BlogPost> getAllPublishedPosts(){
         return blogPostRepository.findByPublishedTrue();
     }
 
-    public Optional<BlogPost> getPostBySlug(String slug){
-        return blogPostRepository.findBySlug(slug);
+    public Optional<BlogPost> getPostBySlug(String slug) {
+        return blogPostRepository.findBySlugAndPublishedTrue(slug);
     }
 
-    public List<BlogPost> getLatestPosts() {
-        return blogPostRepository.findTop3ByPublishedTrueOrderByPublishedAtDesc();
+    public List<BlogPost> getHomepagePosts() {
+        return blogPostRepository.findHomepagePosts();
     }
-
-    //--------------------------------
-    //Admin - ONLY ADMIN CAN ACCESS THIS
-    //--------------------------------
-    public List<BlogPost> getAllPosts(){
-        return blogPostRepository.findAll();
-    }
-
-    public BlogPost createPost(BlogPost blogPost){
-        return blogPostRepository.save(blogPost);
-    }
-
-    public BlogPost updatePost(UUID id, BlogPost updated){
-        BlogPost existing = blogPostRepository.findById(id).orElseThrow(() -> new RuntimeException("Post with id " + id + " not found"));
-
-        existing.setTitle(updated.getTitle());
-        existing.setSlug(updated.getSlug());
-        existing.setContent(updated.getContent());
-        existing.setExcerpt(updated.getExcerpt());
-        existing.setPublished(updated.isPublished());
-        existing.setPublishedAt(updated.getPublishedAt());
-
-        return blogPostRepository.save(existing);
-    }
-
-    public void deletePost(UUID id){
-        blogPostRepository.deleteById(id);
-    }
-
 }
