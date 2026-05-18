3f9459df7cfadfdfc7434f44df2c2fad6dfc9737
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-18 18:02:14 +0100

    refactor: add user column

--- backend-java/src/main/resources/db/migration/V3__create_project_table.sql
+++ backend-java/src/main/resources/db/migration/V3__create_project_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS projects (
-                                        id UUID PRIMARY KEY DEFAULT gen_random_uuid () ,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid () ,
     user_id UUID NOT NULL REFERENCES users ( id ) ,
     title VARCHAR (255) NOT NULL ,
     description TEXT NOT NULL ,
