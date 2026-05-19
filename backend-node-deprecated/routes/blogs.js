11d3320fd1e3996c88d1d6375a609aba99ef7d78
EEEDWARD1 <eduardteodor2004@gmail.com>
2026-05-19 15:12:15 +0100

    chore: remove deprecated node backend and database folder init

--- backend-node-deprecated/routes/blogs.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const blogController = require('../controllers/blogController')
-
-router.get('/', blogController.getPublishedBlogs)
-router.get('/:id', blogController.getById)
-
-module.exports = router
